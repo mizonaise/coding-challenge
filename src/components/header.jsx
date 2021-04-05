b84cb4888e1950d26e9ba3f377682ce64673bf46
mizonaise <mizonaise@gmail.com>
2021-04-05 04:31:03 +0100

    init commit

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <ul>
+          <li>
+            <a href="/">Hello</a>
+          </li>
+          <li>
+            <a href="/">Hello</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
