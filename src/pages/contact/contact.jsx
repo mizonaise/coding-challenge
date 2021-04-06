f6e0e0486f9c1b2933205a03818bdbf8815d119d
mizonaise <mizonaise@gmail.com>
2021-04-06 17:17:55 +0100

    Create contact page

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,7 +1,43 @@
+import "./contact.css";
+
 function Contact() {
   return (
-    <div>
-        <h1>Contact</h1>
+    <div className="contact-container">
+      <div className="contact-inner">
+        <h2>GET IN TOUCH</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <form className="contact-form">
+          <h4>Contact Form</h4>
+          <div className="user-info">
+            <div className="cfield">
+              <label>
+                Name<span>*</span>
+              </label>
+              <input type="text" name="name" required></input>
+            </div>
+            <div className="cfield">
+              <label>
+                Email Address<span>*</span>
+              </label>
+              <input type="email" name="email" required></input>
+            </div>
+          </div>
+          <div className="cfield">
+            <label>
+              Message<span>*</span>
+            </label>
+            <textarea name="subject" required></textarea>
+          </div>
+          <button className="btn-submit" type="submit" value="Clear">
+            {"Send"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
